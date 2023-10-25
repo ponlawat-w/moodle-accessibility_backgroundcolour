7c760825dc463165c96f040289d2c55125244253
Ponlawat W. <ponlawat_w@cmu.ac.th>
2023-10-25 15:05:39 +0700

    Updated coding style

--- styles.php
+++ styles.php
@@ -23,11 +23,13 @@
  */
 
 require_once(__DIR__ . '/../../../../config.php');
-header('Content-Type: text/css');
 
 if (!$USER || !$USER->id) {
     exit;
 }
+require_login(0, true);
+
+header('Content-Type: text/css');
 
 require_once(__DIR__ . '/../../lib.php');
 require_once(__DIR__ . '/accessibility_backgroundcolour.php');
